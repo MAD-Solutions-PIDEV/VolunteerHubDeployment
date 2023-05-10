ff3727cbca516e6e698df2a92b421e3327e47700
almayssa <alwaouimayssa@gmail.com>
2023-05-10 05:31:01 +0100

    fix errors again

--- client/src/pages/hosts.js
+++ client/src/pages/hosts.js
@@ -3,22 +3,16 @@ import Layout from "components/Layout/Layout";
 import HostArea from "components/DisplayHostArea/HostArea";
 import PageTitle from "components/Reuseable/PageTitle";
 import React from "react";
-import { Title } from "chart.js";
 
 const Hosts = () => {
   document.title = "Hosts List";
   return (
     <Layout>
-      
       <Header />
-      <PageTitle title="Hosts List" 
+      <PageTitle title="Hosts List" />
 
-      />
-    
       <HostArea />
-
     </Layout>
-    
   );
 };
 
